4546fe78883b12b122e01d8384b729db2c86d4cd
bryce.t.beddard@vanderbilt.edu <bryce.t.beddard@vanderbilt.edu>
2019-11-20 18:57:58 -0600

    Initial Commit of Word

---
+++ Fascination/Assets/Scripts/LightOn3_0.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOn3_0 : MonoBehaviour
+{
+    public Color altColor = Color.black;
+    public Renderer rend;
+    public GameObject correspondingLight;
+
+    void Start(){
+        correspondingLight = GameObject.Find("Light3_0");
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(correspondingLight != null){
+            rend = correspondingLight.GetComponent<Renderer>();
+            rend.material.color = altColor;
+        }
+    }
+}
---
+++ Fascination/Assets/Scripts/LightOn3_2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOn3_2 : MonoBehaviour
+{
+    public Color altColor = Color.black;
+    public Renderer rend;
+    public GameObject correspondingLight;
+
+    void Start(){
+        correspondingLight = GameObject.Find("Light3_2");
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(correspondingLight != null){
+            rend = correspondingLight.GetComponent<Renderer>();
+            rend.material.color = altColor;
+        }
+    }
+}
