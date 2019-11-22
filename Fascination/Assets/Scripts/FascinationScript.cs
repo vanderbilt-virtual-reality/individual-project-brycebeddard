c857aeedcfa3ef8fea8fa9499db74c02692f09ae
Bryce Beddard <31668545+brycebeddard@users.noreply.github.com>
2019-11-22 00:03:40 -0600

    Final Commit

--- Fascination/Assets/Scripts/FascinationScript.cs
+++ Fascination/Assets/Scripts/FascinationScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
@@ -6,60 +6,88 @@ using UnityEngine;
 public class FascinationScript : MonoBehaviour
 {
     public GameObject light00;
-    public Color rend00;
+    public Renderer rend00;
     public GameObject light01;
-    public Color rend01;
+    public Renderer rend01;
     public GameObject light02;
-    public Color rend02;
+    public Renderer rend02;
     public GameObject light03;
-    public Color rend03;
+    public Renderer rend03;
     public GameObject light04;
-    public Color rend04;
+    public Renderer rend04;
     public GameObject light10;
-    public Color rend10;
+    public Renderer rend10;
     public GameObject light11;
-    public Color rend11;
+    public Renderer rend11;
     public GameObject light12;
-    public Color rend12;
+    public Renderer rend12;
     public GameObject light13;
-    public Color rend13;
+    public Renderer rend13;
     public GameObject light14;
-    public Color rend14;
+    public Renderer rend14;
     public GameObject light20;
-    public Color rend20;
+    public Renderer rend20;
     public GameObject light21;
-    public Color rend21;
+    public Renderer rend21;
     public GameObject light22;
-    public Color rend22;
+    public Renderer rend22;
     public GameObject light23;
-    public Color rend23;
+    public Renderer rend23;
     public GameObject light24;
-    public Color rend24;
+    public Renderer rend24;
     public GameObject light30;
-    public Color rend30;
+    public Renderer rend30;
     public GameObject light31;
-    public Color rend31;
+    public Renderer rend31;
     public GameObject light32;
-    public Color rend32;
+    public Renderer rend32;
     public GameObject light33;
-    public Color rend33;
+    public Renderer rend33;
     public GameObject light34;
-    public Color rend34;
+    public Renderer rend34;
     public GameObject light40;
-    public Color rend40;
+    public Renderer rend40;
     public GameObject light41;
-    public Color rend41;
+    public Renderer rend41;
     public GameObject light42;
-    public Color rend42;
+    public Renderer rend42;
     public GameObject light43;
-    public Color rend43;
+    public Renderer rend43;
     public GameObject light44;
-    public Color rend44;
+    public Renderer rend44;
     public Color altColor = Color.black;
-    public Color winColor = Color.blue;
+    public Color winColor = Color.black;
     public Stopwatch stopwatch;
-    public bool win = true;
-    // Start is called before the first frame update
+    public bool win;
+    void winCelebration(){
+                        UnityEngine.Debug.Log("ENTERED LOOP");
+                        rend00.material.color = winColor;
+                                                rend01.material.color = winColor;
+                                                rend02.material.color = winColor;
+                                                rend03.material.color = winColor;
+                                                rend04.material.color = winColor;
+                                                rend10.material.color = winColor;
+                                                rend11.material.color = winColor;
+                                                rend12.material.color = winColor;
+                                                rend13.material.color = winColor;
+                                                rend14.material.color = winColor;
+                                                rend20.material.color = winColor;
+                                                rend21.material.color = winColor;
+                                                rend22.material.color = winColor;
+                                                rend23.material.color = winColor;
+                                                rend24.material.color = winColor;
+                                                rend30.material.color = winColor;
+                                                rend31.material.color = winColor;
+                                                rend32.material.color = winColor;
+                                                rend33.material.color = winColor;
+                                                rend34.material.color = winColor;
+                                                rend40.material.color = winColor;
+                                                rend41.material.color = winColor;
+                                                rend42.material.color = winColor;
+                                                rend43.material.color = winColor;
+                                                rend44.material.color = winColor;
+                    }
+     //Start is called before the first frame update
     void Start()
     {
         light00 = GameObject.Find("Light0_0");
@@ -87,118 +115,56 @@ public class FascinationScript : MonoBehaviour
         light42 = GameObject.Find("Light4_2");
         light43 = GameObject.Find("Light4_3");
         light44 = GameObject.Find("Light4_4");
+        win = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rend00 = light00.GetComponent<Renderer>().material.color;
-        rend01 = light01.GetComponent<Renderer>().material.color;
-        rend02 = light02.GetComponent<Renderer>().material.color;
-        rend03 = light03.GetComponent<Renderer>().material.color;
-        rend04 = light04.GetComponent<Renderer>().material.color;
-        rend10 = light10.GetComponent<Renderer>().material.color;
-        rend11 = light11.GetComponent<Renderer>().material.color;
-        rend12 = light12.GetComponent<Renderer>().material.color;
-        rend13 = light13.GetComponent<Renderer>().material.color;
-        rend14 = light14.GetComponent<Renderer>().material.color;
-        rend20 = light20.GetComponent<Renderer>().material.color;
-        rend21 = light21.GetComponent<Renderer>().material.color;
-        rend22 = light22.GetComponent<Renderer>().material.color;
-        rend23 = light23.GetComponent<Renderer>().material.color;
-        rend24 = light24.GetComponent<Renderer>().material.color;
-        rend30 = light30.GetComponent<Renderer>().material.color;
-        rend31 = light31.GetComponent<Renderer>().material.color;
-        rend32 = light32.GetComponent<Renderer>().material.color;
-        rend33 = light33.GetComponent<Renderer>().material.color;
-        rend34 = light34.GetComponent<Renderer>().material.color;
-        rend40 = light40.GetComponent<Renderer>().material.color;
-        rend41 = light41.GetComponent<Renderer>().material.color;
-        rend42 = light42.GetComponent<Renderer>().material.color;
-        rend43 = light43.GetComponent<Renderer>().material.color;
-        rend44 = light44.GetComponent<Renderer>().material.color;
-        if((rend00 == altColor && rend01 == altColor && rend02 == altColor && rend03 == altColor && rend04 == altColor) ||
-        (rend10 == altColor && rend11 == altColor && rend12 == altColor && rend13 == altColor && rend14 == altColor) ||
-        (rend20 == altColor && rend21 == altColor && rend22 == altColor && rend23 == altColor && rend24 == altColor) ||
-        (rend30 == altColor && rend31 == altColor && rend32 == altColor && rend33 == altColor && rend34 == altColor) ||
-        (rend40 == altColor && rend41 == altColor && rend42 == altColor && rend43 == altColor && rend44 == altColor) ||
-        (rend00 == altColor && rend10 == altColor && rend20 == altColor && rend30 == altColor && rend40 == altColor) ||
-        (rend01 == altColor && rend11 == altColor && rend21 == altColor && rend31 == altColor && rend41 == altColor) ||
-        (rend02 == altColor && rend12 == altColor && rend22 == altColor && rend32 == altColor && rend42 == altColor) ||
-        (rend03 == altColor && rend13 == altColor && rend23 == altColor && rend33 == altColor && rend43 == altColor) ||
-        (rend04 == altColor && rend14 == altColor && rend24 == altColor && rend34 == altColor && rend44 == altColor) ||
-        (rend00 == altColor && rend11 == altColor && rend22 == altColor && rend33 == altColor && rend44 == altColor) ||
-        (rend04 == altColor && rend13 == altColor && rend22 == altColor && rend31 == altColor && rend40 == altColor))
+        rend00 = light00.GetComponent<Renderer>();
+        rend01 = light01.GetComponent<Renderer>();
+        rend02 = light02.GetComponent<Renderer>();
+        rend03 = light03.GetComponent<Renderer>();
+        rend04 = light04.GetComponent<Renderer>();
+        rend10 = light10.GetComponent<Renderer>();
+        rend11 = light11.GetComponent<Renderer>();
+        rend12 = light12.GetComponent<Renderer>();
+        rend13 = light13.GetComponent<Renderer>();
+        rend14 = light14.GetComponent<Renderer>();
+        rend20 = light20.GetComponent<Renderer>();
+        rend21 = light21.GetComponent<Renderer>();
+        rend22 = light22.GetComponent<Renderer>();
+        rend23 = light23.GetComponent<Renderer>();
+        rend24 = light24.GetComponent<Renderer>();
+        rend30 = light30.GetComponent<Renderer>();
+        rend31 = light31.GetComponent<Renderer>();
+        rend32 = light32.GetComponent<Renderer>();
+        rend33 = light33.GetComponent<Renderer>();
+        rend34 = light34.GetComponent<Renderer>();
+        rend40 = light40.GetComponent<Renderer>();
+        rend41 = light41.GetComponent<Renderer>();
+        rend42 = light42.GetComponent<Renderer>();
+        rend43 = light43.GetComponent<Renderer>();
+        rend44 = light44.GetComponent<Renderer>();
+        if(!win){
+
+        if((rend00.material.color == altColor && rend01.material.color == altColor && rend02.material.color == altColor && rend03.material.color == altColor && rend04.material.color == altColor) ||
+        (rend10.material.color == altColor && rend11.material.color == altColor && rend12.material.color == altColor && rend13.material.color == altColor && rend14.material.color == altColor) ||
+        (rend20.material.color == altColor && rend21.material.color == altColor && rend22.material.color == altColor && rend23.material.color == altColor && rend24.material.color == altColor) ||
+        (rend30.material.color == altColor && rend31.material.color == altColor && rend32.material.color == altColor && rend33.material.color == altColor && rend34.material.color == altColor) ||
+        (rend40.material.color == altColor && rend41.material.color == altColor && rend42.material.color == altColor && rend43.material.color == altColor && rend44.material.color == altColor) ||
+        (rend00.material.color == altColor && rend10.material.color == altColor && rend20.material.color == altColor && rend30.material.color == altColor && rend40.material.color == altColor) ||
+        (rend01.material.color == altColor && rend11.material.color == altColor && rend21.material.color == altColor && rend31.material.color == altColor && rend41.material.color == altColor) ||
+        (rend02.material.color == altColor && rend12.material.color == altColor && rend22.material.color == altColor && rend32.material.color == altColor && rend42.material.color == altColor) ||
+        (rend03.material.color == altColor && rend13.material.color == altColor && rend23.material.color == altColor && rend33.material.color == altColor && rend43.material.color == altColor) ||
+        (rend04.material.color == altColor && rend14.material.color == altColor && rend24.material.color == altColor && rend34.material.color == altColor && rend44.material.color == altColor) ||
+        (rend00.material.color == altColor && rend11.material.color == altColor && rend22.material.color == altColor && rend33.material.color == altColor && rend44.material.color == altColor) ||
+        (rend04.material.color == altColor && rend13.material.color == altColor && rend22.material.color == altColor && rend31.material.color == altColor && rend40.material.color == altColor))
         {
-            while(win)
-            {
-                rend00 = winColor;
-                rend01 = winColor;
-                rend02 = winColor;
-                rend03 = winColor;
-                rend04 = winColor;
-                rend10 = winColor;
-                rend11 = winColor;
-                rend12 = winColor;
-                rend13 = winColor;
-                rend14 = winColor;
-                rend20 = winColor;
-                rend21 = winColor;
-                rend22 = winColor;
-                rend23 = winColor;
-                rend24 = winColor;
-                rend30 = winColor;
-                rend31 = winColor;
-                rend32 = winColor;
-                rend33 = winColor;
-                rend34 = winColor;
-                rend40 = winColor;
-                rend41 = winColor;
-                rend42 = winColor;
-                rend43 = winColor;
-                rend44 = winColor;
-                stopwatch = Stopwatch.StartNew();
-                while(true)
-                {
-                    if(stopwatch.ElapsedMilliseconds >= 1000)
-                    {
-                        break;
-                    }
-                }
-                rend00 = altColor;
-                rend01 = altColor;
-                rend02 = altColor;
-                rend03 = altColor;
-                rend04 = altColor;
-                rend10 = altColor;
-                rend11 = altColor;
-                rend12 = altColor;
-                rend13 = altColor;
-                rend14 = altColor;
-                rend20 = altColor;
-                rend21 = altColor;
-                rend22 = altColor;
-                rend23 = altColor;
-                rend24 = altColor;
-                rend30 = altColor;
-                rend31 = altColor;
-                rend32 = altColor;
-                rend33 = altColor;
-                rend34 = altColor;
-                rend40 = altColor;
-                rend41 = altColor;
-                rend42 = altColor;
-                rend43 = altColor;
-                rend44 = altColor;
-                stopwatch = Stopwatch.StartNew();
-                while(true)
-                {
-                    if(stopwatch.ElapsedMilliseconds >= 1000)
-                    {
-                        break;
-                    }
-                }
-            }
+		    win = true;
+		    UnityEngine.Debug.Log("YAY");
+            winCelebration();
+        }
         }
     }
 }
